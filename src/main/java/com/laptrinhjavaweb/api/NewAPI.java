ba87fb57f40ea94e03d1099012c36669a19e9a30
MatalusMaster <phuocsang.cse@gmail.com>
2020-11-17 00:30:11 +0700

    da sua code

--- src/main/java/com/laptrinhjavaweb/api/NewAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -1,48 +1,45 @@
 package com.laptrinhjavaweb.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.laptrinhjavaweb.dto.NewDTO;
 
 @Controller
 public class NewAPI {
-	NewDTO sang = new NewDTO();
-
-	@RequestMapping(value = "/new", method = RequestMethod.POST)
+	@RequestMapping(value = "/news", method = RequestMethod.POST)
 	@ResponseBody
 	public NewDTO createNew(@RequestBody NewDTO model) {
 		return model;
 	}
 
-	@RequestMapping(value = "/new", method = RequestMethod.PUT)
+	@RequestMapping(value = "/news", method = RequestMethod.PUT)
 	@ResponseBody
 	public NewDTO updateNew(@RequestBody NewDTO model) {
 		return model;
 	}
 
-	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	@GetMapping("/news")
 	@ResponseBody
-	public NewDTO createGet(@RequestBody NewDTO model) {
-
-		sang.setId(12);
-		sang.setTitle("Sang");
-		sang.setContent("SangHo");
-
-		return sang;
+	public String createGet(@RequestParam int id) {
+		return "ID:" + id;
+	}
+	
+	@GetMapping("/news/keyword={keyword}&page={pagenumber}")
+	@ResponseBody
+	public String createGetMulti(@PathVariable(name = "keyword") String keyword,@PathVariable(name = "pagenumber") int pagenumber ) {
+		return "keyword:" + keyword + " pagenumber: " + pagenumber;
 	}
 
-	@RequestMapping(value = "/new", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/news", method = RequestMethod.DELETE)
    @ResponseBody
-   public NewDTO createDelete(@RequestBody NewDTO model) {
-	   sang.setId(0);
-	   return sang;
+   public String createDelete(@RequestParam int id) {
+	   return "ID:" + id;
    }
 }
